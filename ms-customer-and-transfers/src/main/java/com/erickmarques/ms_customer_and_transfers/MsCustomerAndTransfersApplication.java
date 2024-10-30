74c0839c29ea854d9a71ea7315cd88d47640ed9f
Erick Marques <erick.andrade@mv.com.br>
2024-10-30 19:40:28 -0300

    feat: Iniciando projeto de microsserviço de clientes e transferência.

---
+++ ms-customer-and-transfers/src/main/java/com/erickmarques/ms_customer_and_transfers/MsCustomerAndTransfersApplication.java
@@ -0,0 +1,13 @@
+package com.erickmarques.ms_customer_and_transfers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCustomerAndTransfersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCustomerAndTransfersApplication.class, args);
+	}
+
+}
---
+++ ms-customer-and-transfers/src/test/java/com/erickmarques/ms_customer_and_transfers/MsCustomerAndTransfersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erickmarques.ms_customer_and_transfers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCustomerAndTransfersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
