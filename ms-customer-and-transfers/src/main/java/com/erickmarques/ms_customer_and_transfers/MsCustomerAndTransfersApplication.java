d4e7d2c3067cc7c4a06dab7b446bb3577bbe0401
Erick Marques <erick.andrade@mv.com.br>
2024-10-30 23:21:39 -0300

    feat: Habilitando JPA Audting e feign clients e criando conexao com o banco local.

--- ms-customer-and-transfers/src/main/java/com/erickmarques/ms_customer_and_transfers/MsCustomerAndTransfersApplication.java
+++ ms-customer-and-transfers/src/main/java/com/erickmarques/ms_customer_and_transfers/MsCustomerAndTransfersApplication.java
@@ -2,8 +2,12 @@ package com.erickmarques.ms_customer_and_transfers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableFeignClients
 public class MsCustomerAndTransfersApplication {
 
 	public static void main(String[] args) {
