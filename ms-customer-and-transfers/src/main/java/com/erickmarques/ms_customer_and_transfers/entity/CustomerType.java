5b2f7b16dce4de84c0b77eb786bb833a136a9508
Erick Marques <erick.andrade@mv.com.br>
2024-10-30 21:24:36 -0300

    feat: Adicionando enum para o tipo de cliente(comum ou lojista).

--- ms-customer-and-transfers/src/main/java/com/erickmarques/ms_customer_and_transfers/entity/CustomerType.java
+++ ms-customer-and-transfers/src/main/java/com/erickmarques/ms_customer_and_transfers/entity/CustomerType.java
@@ -3,11 +3,13 @@ package com.erickmarques.ms_customer_and_transfers.entity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tipos_cliente")
 @Builder
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CustomerType {
@@ -19,4 +21,8 @@ public class CustomerType {
 
     @Column(name = "name", nullable = false)
     private String name;
+
+    @Column(name = "user_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
 }
---
+++ ms-customer-and-transfers/src/main/java/com/erickmarques/ms_customer_and_transfers/entity/UserType.java
@@ -0,0 +1,6 @@
+package com.erickmarques.ms_customer_and_transfers.entity;
+
+public enum UserType {
+    REGULAR_USER,
+    MERCHANT
+}
